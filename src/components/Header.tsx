c1f645dcc86c9026c7e96e800b01602586c29683
Jonas Wingård <jonas.wingard@gmail.com>
2023-01-27 11:54:16 +0100

    tailwind and header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,39 @@
 import { FC } from "react";
 import Link from "next/link";
 import { useSession, signIn, signOut } from "next-auth/react";
+import Image from "next/image";
 
 const Header: FC = () => {
   const { data: session } = useSession();
 
   return (
-    <header>
+    <header className="p-6 flex justify-between text-white bg-slate-800">
       <Link href="/">
-        <span className="logo">AppLogo</span>
+        <span className="logo">🍕🍔🌮</span>
       </Link>
       {session && (
-        <Link
-          href="#"
-          onClick={(e) => {
-            e.preventDefault();
-            signOut();
-          }}
-          className="btn-signin"
-        >
-          SIGN OUT
-        </Link>
+        <div className="flex items-center">
+          <div className="mr-1 rounded-full h-5 w-5 overflow-hidden">
+            <Image
+              width={20}
+              height={20}
+              src={session.user?.image ?? ""}
+              alt=""
+            />
+          </div>
+          {session.user?.name ?? session.user?.email}
+          <div className="px-3">{" | "}</div>
+          <Link
+            href="#"
+            onClick={(e) => {
+              e.preventDefault();
+              signOut();
+            }}
+            className="btn-signin"
+          >
+            Sign out
+          </Link>
+        </div>
       )}
       {!session && (
         <Link
@@ -31,7 +44,7 @@ const Header: FC = () => {
           }}
           className="btn-signin"
         >
-          SIGN IN
+          Sign in
         </Link>
       )}
     </header>
