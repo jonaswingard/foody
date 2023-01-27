f1db765e3b9a79277b0973def3c307607269db62
Jonas Wingård <jonas.wingard@gmail.com>
2023-01-27 14:45:31 +0100

    added secret

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,8 +2,16 @@ import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 import { NextApiRequest, NextApiResponse } from "next";
 
-if (!process.env.GITHUB_ID || !process.env.GITHUB_SECRET) {
-  throw new Error("GITHUB_ID and GITHUB_SECRET must be set");
+console.log("que");
+
+if (
+  !process.env.GITHUB_ID ||
+  !process.env.GITHUB_SECRET ||
+  !process.env.NEXT_PUBLIC_SECRET
+) {
+  throw new Error(
+    "GITHUB_ID, GITHUB_SECRET and NEXT_PUBLIC_SECRET must be set"
+  );
 }
 
 const options = {
@@ -13,6 +21,7 @@ const options = {
       clientSecret: process.env.GITHUB_SECRET ?? "",
     }),
   ],
+  secret: process.env.NEXT_PUBLIC_SECRET,
 };
 
 export default (req: NextApiRequest, res: NextApiResponse) =>
